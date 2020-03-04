4b6bd6794b39d675aa83c2ae82d4892a42be2683
anya0428 <59860463+anya0428@users.noreply.github.com>
2020-03-04 15:34:35 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-// Scroll function courtesy of Scott Dowding; http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
-
-$(document).ready(function() {
-  // Check if element is scrolled into view
-  function isScrolledIntoView(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-
-    return elemBottom <= docViewBottom && elemTop >= docViewTop;
-  }
-  // If element is scrolled into view, fade it in
-  $(window).scroll(function() {
-    $(".scroll-animations .animated").each(function() {
-      if (isScrolledIntoView(this) === true) {
-        $(this).addClass("fadeInLeft");
-      }
-    });
-  });
-
-  
+$(".panel-heading").parent('.panel').hover(
+function() {
+$(this).children('.collapse').collapse('show');
+}, function() {
+$(this).children('.collapse').collapse('hide');
+}
+);
